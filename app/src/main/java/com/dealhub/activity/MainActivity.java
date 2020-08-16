e209a4fae594638f74959970c829cda686b75185
Anuradi <chamodianuradi@gmail.com>
2020-08-16 16:42:54 +0530

    create  project structure bottom navigation code create fragments add colors back end code bottom navigation add resource

---
+++ app/src/main/java/com/dealhub/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package com.dealhub.activity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.dealhub.R;
+import com.dealhub.fragment.favorite_customer;
+import com.dealhub.fragment.offers_customer;
+import com.dealhub.fragment.profile_customer;
+import com.dealhub.fragment.shops_customer;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
+    static String id;
+    public  Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        id = getIntent().getStringExtra("id");
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch (menuItem.getItemId()) {
+                case R.id.nav_shop:
+                    selectedFragment = new shops_customer();
+                    break;
+
+                case R.id.nav_offers:
+                    selectedFragment = new offers_customer();
+                    break;
+
+                case R.id.nav_favorite:
+                    selectedFragment = new favorite_customer();
+                    break;
+
+                case R.id.nav_profile:
+                    selectedFragment = new profile_customer();
+                    break;
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
+        }
+
+
+    };
+}
---
+++ app/src/main/java/com/dealhub/fragment/favorite_customer.java
@@ -0,0 +1,62 @@
+package com.dealhub.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dealhub.R;
+
+
+public class favorite_customer extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public favorite_customer() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment favorite_customer.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static favorite_customer newInstance(String param1, String param2) {
+        favorite_customer fragment = new favorite_customer();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+}
