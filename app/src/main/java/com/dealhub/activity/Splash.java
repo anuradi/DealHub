72a4d9f217025f9d9f80460336ee0799b9255ab7
Anuradi <chamodianuradi@gmail.com>
2020-09-22 19:27:24 +0530

    add Splash screen

---
+++ app/src/main/java/com/dealhub/activity/Splash.java
@@ -0,0 +1,16 @@
+package com.dealhub.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dealhub.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
