f99aa5ad3983a58cb2a80025feb7225a4548115a
Hasitha20 <hasithasaranga1999@gmail.com>
2020-09-24 13:17:34 +0530

    Add Cart_Adapter Update coupon add drawable

---
+++ app/src/main/java/com/dealhub/adapters/Cart_Adapter.java
@@ -0,0 +1,16 @@
+package com.dealhub.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dealhub.R;
+
+public class Cart_Adapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart__adapter);
+    }
+}
